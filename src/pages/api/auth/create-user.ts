840ce2e601022acacbbfed6b13da54cf0dc25413
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2026-01-23 02:56:12 +0530

    changed dir

--- src/pages/api/auth/create-user.ts
+++ src/pages/api/auth/create-user.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
-import { checkIfPostOrSetError } from "../utils";
+import { checkIfPostOrSetError } from "../../../lib/api/utils";
 import { getUserIdOrSetError } from "../../../lib/auth/authUtils";
 
 export default async function handler(
